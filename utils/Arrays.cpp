0d0819cf2553189a4cc5d47d9b451784b688909e
Isaac John Wahing <therealcaasi@gmail.com>
2021-05-12 17:11:29 +0800

    feat: Implementation of Arrays

--- utils/Arrays.cpp
+++ utils/Arrays.cpp
@@ -1,16 +1,43 @@
 #include <iostream>
 
+using namespace std;
+
 template <typename T>
 class Array {
 private:
-    T *ptr;
+    T *data;
     int size;
 public:
-    Array(T arr[], int s);
-    void print();
+
+    Array(int size){
+        assert(size > 0);
+        data = new T[size]{};
+        size = size;
+    }
+
+    Array(const Array&) = delete;
+    Array& operator=(const Array&) = delete;
+
+    ~Array(){
+        delete[] data;
+    }
+
+    void erase(){
+        delete[] data;
+        data = nullptr;
+        size = 0;
+    }
+
+    T& operator[](int index){
+        assert(index >= 0 && index < size);
+        return data[index];
+    }
+
+    int getSize() const {
+        return size;
+    }
 };
 
-int main()
-{
+int main(){
     return 0;
 }
